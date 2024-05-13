8d44a4edc1a4cff04ded91b9bfd24d9e70f927a0
Vadim Malinovskii <overrated322@mail.ru>
2024-05-14 00:29:16 +0300

    First try

--- scripts/index.js
+++ scripts/index.js
@@ -1,10 +1,10 @@
-// DOM узлы
+//Темплейт карточки
 const cardTemplate = document.querySelector("#card-template").content;
+// DOM узлы
 const placesList = document.querySelector(".places__list");
 
 //Функция создания карточки
 let addCard = (cardData, { deleteCard }) => {
-    //Темплейт карточки
   const cardElement = cardTemplate.querySelector(".card").cloneNode(true);
 
   cardElement.querySelector(".card__image").src = cardData.link;
@@ -12,22 +12,24 @@ let addCard = (cardData, { deleteCard }) => {
 
   cardElement
     .querySelector(".card__delete-button")
-    .addEventListener("click", () => {deleteCard(cardElement)});
+    .addEventListener("click", () => {
+      deleteCard(cardElement);
+    });
 
   return cardElement;
-}
+};
 //Функция удаления карточки
 let deleteCard = (cardElement) => {
-    let cards = document.querySelectorAll('.card');
-    for (let i = 0; i < cards.length; i++) {
-        if (cards[i] == cardElement) {
-            cards[i].remove();
-            break
-        }
-      }
-}
+  let cards = document.querySelectorAll(".card");
+  for (let i = 0; i < cards.length; i++) {
+    if (cards[i] == cardElement) {
+      cards[i].remove();
+      break;
+    }
+  }
+};
 
 //Вывести карточки на страницу
 initialCards.forEach((item) => {
-    placesList.append(addCard(item, { deleteCard }));
+  placesList.append(addCard(item, { deleteCard }));
 });
